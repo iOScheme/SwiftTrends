8f687737f06884a8149eda3df3fba46c115b5145
AlexApostol <apostol516@gmail.com>
2024-08-18 17:16:48 +0200

    Initial Commit

---
+++ SwiftTrends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTrends
+//
+//  Created by Alex.personal on 18/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTrends/SwiftTrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTrendsApp.swift
+//  SwiftTrends
+//
+//  Created by Alex.personal on 18/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
